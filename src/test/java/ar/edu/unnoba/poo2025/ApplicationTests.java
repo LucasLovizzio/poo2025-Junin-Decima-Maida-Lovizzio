96aa12253e11af0457e2f2d261ecaf99403a891b
Lucas <llovizzio@comunidad.unnoba.edu.ar>
2025-10-25 00:06:50 -0300

    Cambio de nombre y ubicacion de la SpringBootAppliation.

--- src/test/java/ar/edu/unnoba/poo2025/ApplicationTests.java
+++ src/test/java/ar/edu/unnoba/poo2025/ApplicationTests.java
@@ -1,13 +1,12 @@
-package ar.edu.unnoba.poo2025.torneos;
+package ar.edu.unnoba.poo2025;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
