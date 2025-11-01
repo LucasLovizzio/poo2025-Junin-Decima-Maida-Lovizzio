3b195e4ed97ced45d613b8c7fccf83ee0da1aa27
Lucas <llovizzio@comunidad.unnoba.edu.ar>
2025-11-01 11:57:29 -0300

    fix tabs

--- src/main/java/ar/edu/unnoba/poo2025/torneos/dto/CreateParticipantResponseDTO.java
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/dto/CreateParticipantResponseDTO.java
@@ -2,26 +2,26 @@ package ar.edu.unnoba.poo2025.torneos.dto;
 
 public class CreateParticipantResponseDTO {
 
-        private Long id;
-        private String email;
+    private Long id;
+    private String email;
 
-        public CreateParticipantResponseDTO() {
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public CreateParticipantResponseDTO() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
 
