d81c3ff38a281dea636639b42f5f00329bf37140
Melinamaida <mmaida886@comunidad.unnoba.edu.ar>
2025-11-01 11:39:44 -0300

    borre List

--- src/main/java/ar/edu/unnoba/poo2025/torneos/resource/ParticipantResource.java
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/resource/ParticipantResource.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/participants")
 
--- src/main/java/ar/edu/unnoba/poo2025/torneos/service/ParticipantService.java
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/service/ParticipantService.java
@@ -4,8 +4,6 @@ import ar.edu.unnoba.poo2025.torneos.exception.ParticipantAlreadyExistsException
 import ar.edu.unnoba.poo2025.torneos.exception.ParticipantNotFoundException;
 import ar.edu.unnoba.poo2025.torneos.model.Participant;
 
-import java.util.List;
-
 public interface ParticipantService {
 
     Participant create(Participant p) throws ParticipantAlreadyExistsException;
