8ada86feceedaed2ba8f9e541fd8f2453632de98
Lucas <llovizzio@comunidad.unnoba.edu.ar>
2025-11-04 18:16:48 -0300

    añade exceptions de authentication y authorization, tambien las añade a el GlobalExceptionHandler

---
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/exception/AuthenticationFailedException.java
@@ -0,0 +1,21 @@
+package ar.edu.unnoba.poo2025.torneos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class AuthenticationFailedException extends RuntimeException {
+
+    private static final HttpStatusCode code = HttpStatus.UNAUTHORIZED;
+
+    public AuthenticationFailedException() {
+        super("Authentication failed: Invalid credentials");
+    }
+
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+
+    public HttpStatusCode getCode() {
+        return code;
+    }
+}
---
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/exception/AuthorizationFailedException.java
@@ -0,0 +1,21 @@
+package ar.edu.unnoba.poo2025.torneos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class AuthorizationFailedException extends RuntimeException {
+
+    private static final HttpStatusCode code = HttpStatus.FORBIDDEN;
+
+    public AuthorizationFailedException() {
+        super("Authorization failed: Invalid or expired token");
+    }
+
+    public AuthorizationFailedException(String message) {
+        super(message);
+    }
+
+    public HttpStatusCode getCode() {
+        return code;
+    }
+}
