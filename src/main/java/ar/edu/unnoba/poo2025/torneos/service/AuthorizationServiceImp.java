50eefb16369da436a817d93df82a089d37f98ed7
Ulises <ulises.decima@avvale.com>
2025-11-10 15:02:24 -0300

    fix and improve

--- src/main/java/ar/edu/unnoba/poo2025/torneos/service/AuthorizationServiceImp.java
+++ src/main/java/ar/edu/unnoba/poo2025/torneos/service/AuthorizationServiceImp.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthotizationServiceImp {
+public class AuthorizationServiceImp implements AuthorizationService {
 
 	private final JwtTokenUtil jwtTokenUtil;
 	private final ParticipantService participantService;
